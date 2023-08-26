542bb1184618e72b30f1c18003f77fd02c47e9da
Valeriia-Trytiak <valeriia.trytiak@gmail.com>
2023-08-26 09:06:24 +0200

    update

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,17 +1,15 @@
-// const params = new URLSearchParams {
-
-// }
 import axios from 'axios';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
+axios.defaults.baseURL = 'https://api.thecatapi.com';
 axios.defaults.headers.common['x-api-key'] =
   'live_ A3ZSRujL9i0dmt1zQPz6xhOQWX9GuL do6JWDcBab6VSCSuKeM20Dut29LtXj fc01';
 
 function fetchBreeds() {
   return axios
-    .get('https://api.thecatapi.com/v1/breeds')
+    .get('/v1/breeds')
     .then(response => {
-      console.log(response);
+      return response.data;
     })
     .catch(error => {
       console.log(error);
@@ -19,4 +17,17 @@ function fetchBreeds() {
     });
 }
 
-export { fetchBreeds };
+function fetchCatByBreed(breedId) {
+  return axios
+    .get(`/v1/images/search?breed_ids=${breedId}`)
+    .then(response => {
+      console.log(response);
+      return response;
+    })
+    .catch(error => {
+      console.log(error);
+      Notify.failure(`${error.message}`);
+    });
+}
+
+export { fetchBreeds, fetchCatByBreed };
--- src/index.js
+++ src/index.js
@@ -1,10 +1,32 @@
-import { fetchBreeds } from './cat-api.js';
+import axios from 'axios';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import { fetchBreeds, fetchCatByBreed } from './cat-api.js';
+
 const refs = {
   breedList: document.querySelector('.breed-select'),
 };
-// console.log(refs.breedList);
 
-// function markupList() {}
+refs.breedList.addEventListener('change', onChangeBreedList);
 
-console.log(fetchBreeds());
+fetchBreeds()
+  .then(data => {
+    createOption(data);
+    refs.breedList.insertAdjacentHTML('beforeend', createOption(data));
+  })
+  .catch(error => {
+    console.log(error);
+    Notify.failure(`${error.message}`);
+  });
+
+function createOption(arr) {
+  return arr
+    .map(({ id, name }) => {
+      return `<option value="${id}">${name}</option>;`;
+    })
+    .join('');
+}
+
+function onChangeBreedList(evt) {
+  console.log(evt.target.value);
+  fetchCatByBreed(evt.target.value).then(value => {});
+}
