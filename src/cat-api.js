5839669f5c08c0c3f111ab14d1c96055fe6a506f
Valeriia-Trytiak <valeriia.trytiak@gmail.com>
2023-08-27 08:55:45 +0200

    update

--- src/cat-api.js
+++ src/cat-api.js
@@ -3,7 +3,7 @@ import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 axios.defaults.baseURL = 'https://api.thecatapi.com';
 axios.defaults.headers.common['x-api-key'] =
-  'live_ A3ZSRujL9i0dmt1zQPz6xhOQWX9GuL do6JWDcBab6VSCSuKeM20Dut29LtXj fc01';
+  'live_A3ZSRujL9i0dmt1zQPz6xhOQWX9GuLdo6JWDcBab6VSCSuKeM20Dut29LtXjfc01';
 
 function fetchBreeds() {
   return axios
@@ -13,7 +13,7 @@ function fetchBreeds() {
     })
     .catch(error => {
       console.log(error);
-      Notify.failure(`${error.message}`);
+      Notify.failure(`Oops! Something went wrong! Try reloading the page!`);
     });
 }
 
@@ -21,12 +21,11 @@ function fetchCatByBreed(breedId) {
   return axios
     .get(`/v1/images/search?breed_ids=${breedId}`)
     .then(response => {
-      console.log(response);
-      return response;
+      return response.data;
     })
     .catch(error => {
       console.log(error);
-      Notify.failure(`${error.message}`);
+      Notify.failure(`Oops! Something went wrong! Try reloading the page!`);
     });
 }
 
--- src/index.js
+++ src/index.js
@@ -2,8 +2,11 @@ import axios from 'axios';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import { fetchBreeds, fetchCatByBreed } from './cat-api.js';
 
+import SlimSelect from 'slim-select';
+
 const refs = {
   breedList: document.querySelector('.breed-select'),
+  container: document.querySelector('.cat-info'),
 };
 
 refs.breedList.addEventListener('change', onChangeBreedList);
@@ -12,6 +15,13 @@ fetchBreeds()
   .then(data => {
     createOption(data);
     refs.breedList.insertAdjacentHTML('beforeend', createOption(data));
+
+    new SlimSelect({
+      select: refs.breedList,
+      settings: {
+        placeholderText: 'Please, select a cat breed',
+      },
+    });
   })
   .catch(error => {
     console.log(error);
@@ -28,5 +38,23 @@ function createOption(arr) {
 
 function onChangeBreedList(evt) {
   console.log(evt.target.value);
-  fetchCatByBreed(evt.target.value).then(value => {});
+  fetchCatByBreed(evt.target.value)
+    .then(data => {
+      console.dir(data);
+      createCarts(data);
+      refs.container.insertAdjacentHTML('beforeend', createCarts(data));
+    })
+    .catch(error => {
+      console.log(error);
+      Notify.failure(`${error.message}`);
+    });
+}
+
+function createCarts(arr) {
+  const { url } = arr[0];
+  const { name: nameCat, description, temperament } = arr[0].breeds[0];
+  return `<img src="${url}" alt="${nameCat}" />
+<h2>${nameCat}</h2>
+<p>${description}</p>
+<p><span>Temperament:</span> ${temperament}</p>`;
 }
